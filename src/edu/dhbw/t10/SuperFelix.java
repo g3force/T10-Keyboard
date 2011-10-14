b947c4a8d070991a9ed4aca3656bc45b98574252
Nicolai Ommer <nicolai.ommer@gmail.com>
2011-10-14 12:12:07 +0200

    Eclipse Project and main Java class

---
+++ src/edu/dhbw/t10/SuperFelix.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.dhbw.t10;
+
+/**
+ * @author NicolaiO
+ *
+ */
+public class SuperFelix {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
