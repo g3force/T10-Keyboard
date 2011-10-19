fbd9b0be29cb6037a2c6404fef08e91af5220326
PEACE121 <dirk@klm-on.de>
2011-10-20 00:37:29 +0200

    started to implement the priority tree added PriorityElement Class added method insert and getWord in PriorityTree have to be tested

--- src/edu/dhbw/t10/type/PriorityTree.java
+++ src/edu/dhbw/t10/type/PriorityTree.java
@@ -22,18 +22,88 @@ public class PriorityTree
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
-	
+	private PriorityElement	root;
 	
 	// --------------------------------------------------------------------------
 	// --- constructors ---------------------------------------------------------
 	// --------------------------------------------------------------------------
-	
+	public PriorityTree()
+	{
+		root = new PriorityElement('\u0000', null, null, 0);
+	}
 	
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
 	
+	public void insert(String in)
+	{
+		PriorityElement node = root.clone();
+		char[] inChar = in.toCharArray(); //put every letter of the word alone in an char array
+		for(int i=0; i<inChar.length; i++)
+		{
+			if (node.hasFollower(inChar[i]))
+			{
+				if (i < inChar.length - 1)
+				{
+					node = node.getFollower(inChar[i]);
+				} else
+				{
+					node.increase();
+					ReplaceSuggestsInFathers(node);
+				}
+			}
+			else {
+				node = node.addFollower(inChar[i]);
+			}
+		}
+	}
 	
+	
+	private void ReplaceSuggestsInFathers(PriorityElement node)
+	{
+		int freq = node.getFrequency();
+		while(node.getFather().getSuggest().getFrequency()<freq)
+		{
+			node.getFather().setSuggest(node);
+			node = node.getFather();
+		}
+	}
+
+
+	public String getSuggest(String wordPart)
+	{
+		PriorityElement node = root.clone();
+		char[] wordPartChar = wordPart.toCharArray(); // put every letter of the word alone in a char array
+		for(int i=0; i<wordPartChar.length; i++)
+		{
+			if (node.hasFollower(wordPartChar[i]))
+			{
+				if (i < wordPartChar.length - 1)
+				{
+					node = node.getFollower(wordPartChar[i]);
+				} else
+				{
+					return node.buildWord();
+				}
+			}
+ else
+			{
+				return wordPart;
+			}
+		}
+		return "FAILURE";
+	}
+	
+	
+	public void printTree()
+	{
+		// TODO a print method to test insert and getWord
+	}
+	
+	
+
+
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
