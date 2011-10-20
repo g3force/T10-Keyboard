8ceeba08d596e61e07f1a8b74f3a536356e12ec7
Nicolai Ommer <nicolai.ommer@gmail.com>
2011-10-20 18:34:34 +0200

    first GUI buttons from xml file :)

--- src/edu/dhbw/t10/type/Key.java
+++ src/edu/dhbw/t10/type/Key.java
@@ -2,21 +2,14 @@
  * *********************************************************
  * Copyright (c) 2011 - 2011, DHBW Mannheim
  * Project: T10 On-Screen Keyboard
- * Date: Oct 18, 2011
+ * Date: Oct 20, 2011
  * Author(s): NicolaiO
  * 
  * *********************************************************
  */
-package edu.dhbw.t10.view.panels;
+package edu.dhbw.t10.type;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-import edu.dhbw.t10.manager.profile.LayoutFileManager;
-import edu.dhbw.t10.type.Key;
 
 
 /**
@@ -27,54 +20,93 @@ import edu.dhbw.t10.type.Key;
  * @author NicolaiO
  * 
  */
-public class MainPanel extends JPanel {
+public class Key {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
+	private String		name		= "";
+	private int			id			= 0;
+	private String		keycode	= "";
+	private Dimension	size		= new Dimension(0, 0);
+	private int			pos_x		= 0;
+	private int			pos_y		= 0;
 	
-	private static final long	serialVersionUID	= -52892520461804389L;
-	
-	
-	// private KeyboardPanel keyboardPanel;
-	// private MutePanel mutePanel;
-	// private ProfilePanel profilePanel;
-
-
 	// --------------------------------------------------------------------------
 	// --- constructors ---------------------------------------------------------
 	// --------------------------------------------------------------------------
-	
-	public MainPanel() {
-		this.setLayout(new FlowLayout());
-		JButton btn = new JButton();
-		LayoutFileManager lfm = new LayoutFileManager();
-		for (Key key : lfm.getKeys()) {
-			btn = new JButton();
-			btn.setLayout(null);
-			btn.setPreferredSize(new Dimension(100, 50));
-			btn.setText(key.getName());
-			this.add(btn);
-		}
-		
-
-		// this.setLayout(new BorderLayout());
-		// this.setSize(300, 150);
-		// keyboardPanel = new KeyboardPanel();
-		// mutePanel = new MutePanel();
-		// profilePanel = new ProfilePanel();
-		// this.add(keyboardPanel, BorderLayout.SOUTH);
-		// // TODO new Layout
-		// this.add(mutePanel, BorderLayout.NORTH);
-		// this.add(profilePanel, BorderLayout.NORTH);
+	public Key(int id, String name, String keycode, Dimension size, int pos_x, int pos_y) {
+		this.name = name;
+		this.id = id;
+		this.keycode = keycode;
+		this.size = size;
+		this.pos_x = pos_x;
+		this.pos_y = pos_y;
 	}
-	
 
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
 	
-
+	
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public String getKeycode() {
+		return keycode;
+	}
+	
+	
+	public void setKeycode(String keycode) {
+		this.keycode = keycode;
+	}
+	
+	
+	public Dimension getSize() {
+		return size;
+	}
+	
+	
+	public void setSize(Dimension size) {
+		this.size = size;
+	}
+	
+	
+	public int getPos_x() {
+		return pos_x;
+	}
+	
+	
+	public void setPos_x(int pos_x) {
+		this.pos_x = pos_x;
+	}
+	
+	
+	public int getPos_y() {
+		return pos_y;
+	}
+	
+	
+	public void setPos_y(int pos_y) {
+		this.pos_y = pos_y;
+	}
 }
