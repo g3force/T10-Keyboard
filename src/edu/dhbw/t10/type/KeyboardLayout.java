bdca6375a6c02ce51d7ad1c0ee908fe11e3f8a31
Nicolai Ommer <g3force@online.de>
2011-10-21 18:39:32 +0200

    added Keyboard scale-factor. Rescaling with window not done yet. Added some more keys. delete some obsolete Panel classes and refactored LayoutFileManger to KeyboardLayoutGenerator
    Change-Id: Ia8c3aac5f848cfdf76bd1ba2f900929c089920f3

--- src/edu/dhbw/t10/type/KeyboardLayout.java
+++ src/edu/dhbw/t10/type/KeyboardLayout.java
@@ -1,15 +1,16 @@
-/*
+/* 
  * *********************************************************
  * Copyright (c) 2011 - 2011, DHBW Mannheim
  * Project: T10 On-Screen Keyboard
- * Date: Oct 18, 2011
+ * Date: Oct 21, 2011
  * Author(s): NicolaiO
- * 
+ *
  * *********************************************************
  */
-package edu.dhbw.t10.view.panels;
+package edu.dhbw.t10.type;
 
-import javax.swing.JPanel;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 
 /**
@@ -20,23 +21,83 @@ import javax.swing.JPanel;
  * @author NicolaiO
  * 
  */
-public class MutePanel extends JPanel {
+public class KeyboardLayout {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
-	private static final long	serialVersionUID	= 4534874484619775498L;
+	private ArrayList<Key>	keys		= new ArrayList<Key>();
+	private int					size_x	= 0;
+	private int					size_y	= 0;
+	private float				scale		= 1;
 	
 	// --------------------------------------------------------------------------
 	// --- constructors ---------------------------------------------------------
 	// --------------------------------------------------------------------------
+	public KeyboardLayout(int size_x, int size_y, float scale) {
+		this.size_x = size_x;
+		this.size_y = size_y;
+		this.scale = scale;
+	}
 	
-
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
+	public void add(Key key) {
+		keys.add(key);
+	}
 	
-
+	
+	public void rescale() {
+		for (Key k : keys) {
+			Rectangle rect = k.getBounds();
+			rect.setBounds((int) (k.getPos_x() * scale), (int) (k.getPos_y() * scale), (int) (k.getSize().width * scale),
+					(int) (k.getSize().height * scale));
+			k.setBounds(rect);
+		}
+	}
+	
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
+	
+	public ArrayList<Key> getKeys() {
+		return keys;
+	}
+	
+	
+	public void setKeys(ArrayList<Key> keys) {
+		this.keys = keys;
+	}
+	
+	
+	public int getSize_x() {
+		return (int) (size_x * scale);
+	}
+	
+	
+	public void setSize_x(int size_x) {
+		this.size_x = size_x;
+	}
+	
+	
+	public int getSize_y() {
+		return (int) (size_y * scale);
+	}
+	
+	
+	public void setSize_y(int size_y) {
+		this.size_y = size_y;
+	}
+	
+	
+	public float getScale() {
+		return scale;
+	}
+	
+	
+	public void setScale(int scale) {
+		this.scale = scale;
+	}
+
+
 }
