5dad7004b4b7450816b19132adb0f5f7ba9b1155
Nicolai Ommer <nicolai.ommer@gmail.com>
2011-10-21 08:47:09 +0200

    keyboard layout extended, F-keys added

--- src/edu/dhbw/t10/manager/profile/LayoutFileManager.java
+++ src/edu/dhbw/t10/manager/profile/LayoutFileManager.java
@@ -9,7 +9,6 @@
  */
 package edu.dhbw.t10.manager.profile;
 
-import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,6 +17,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -40,6 +40,7 @@ public class LayoutFileManager {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
+	private static final Logger		logger	= Logger.getLogger(LayoutFileManager.class);
 	public static final String			KEY		= "key";
 	private File							layoutFile;
 	private DocumentBuilderFactory	dbFactory;
@@ -78,9 +79,10 @@ public class LayoutFileManager {
 			dBuilder = dbFactory.newDocumentBuilder();
 			reload();
 		} catch (ParserConfigurationException err) {
-			// TODO Auto-generated catch block
+			logger.error("Could not initialize dBuilder");
 			err.printStackTrace();
 		}
+		logger.debug("LayoutFileManager initialized");
 	}
 	
 
@@ -97,13 +99,20 @@ public class LayoutFileManager {
 			doc.getDocumentElement().normalize();
 			NodeList nList = doc.getElementsByTagName(KEY);
 			keys = new ArrayList<Key>();
+
 			for (int temp = 0; temp < nList.getLength(); temp++) {
 				Node nNode = nList.item(temp);
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
-					keys.add(getKey(eElement));
+					Key newKey = getKey(eElement);
+					if (newKey != null) {
+						keys.add(newKey);
+					}
+				} else {
+					logger.warn("key-node is not an element-node");
 				}
 			}
+			logger.debug("Loaded " + keys.size() + " keys.");
 			return keys.size();
 		} catch (SAXException err) {
 			// TODO Auto-generated catch block
@@ -129,25 +138,48 @@ public class LayoutFileManager {
 	private Key getKey(Element eElement) {
 		try {
 			NamedNodeMap attr = eElement.getAttributes();
-			Key key = new Key(getIntAttribute(attr, "id"), getAttribute(attr, "name"), getAttribute(attr, "keycode"),
-					new Dimension(getIntAttribute(attr, "size_x"), getIntAttribute(attr, "size_y")), getIntAttribute(attr,
-							"pos_x"), getIntAttribute(attr, "pos_y"));
+			Key key = new Key(getIntAttribute(attr, "size_x"), getIntAttribute(attr, "size_y"), getIntAttribute(attr,
+					"pos_x"), getIntAttribute(attr, "pos_y"));
+			
+			NodeList modes = eElement.getElementsByTagName("mode");
+			for (int i = 0; i < modes.getLength(); i++) {
+				Node item = modes.item(i);
+				if (item != null) {
+					Node keycode = item.getAttributes().getNamedItem("keycode");
+					Node modeName = item.getAttributes().getNamedItem("name");
+					if (keycode != null && modeName != null) {
+						key.addMode(modeName.getTextContent(), item.getTextContent(), keycode.getTextContent());
+					} else {
+						logger.warn("key-node could not be read. Item: " + item.getTextContent());
+					}
+				}
+			}
+			
 			return key;
 		} catch (NullPointerException e) {
 			System.out.println("In getKey:");
 			e.printStackTrace();
-			return null;
 		}
+		return new Key();
 	}
 	
 
 	private String getAttribute(NamedNodeMap attr, String name) throws NullPointerException {
-		return attr.getNamedItem(name).getTextContent();
+		Node node = attr.getNamedItem(name);
+		if (node != null) {
+			return node.getTextContent();
+		}
+		return "";
 	}
 	
 
 	private int getIntAttribute(NamedNodeMap attr, String name) throws NullPointerException {
-		return Integer.parseInt(attr.getNamedItem(name).getTextContent());
+		try {
+			int value = Integer.parseInt(getAttribute(attr, name));
+			return value;
+		} catch (NumberFormatException e) {
+			return 0;
+		}
 	}
 	
 	
