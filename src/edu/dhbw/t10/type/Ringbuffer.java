3cd749dd3db21572668b9caa2e7ea126b7f9e9d8
PEACE121 <dirk@klm-on.de>
2011-10-24 13:59:55 +0200

    Serializer added

---
+++ src/edu/dhbw/t10/type/Ringbuffer.java
@@ -0,0 +1,48 @@
+package edu.dhbw.t10.type;
+public class Ringbuffer<T> {
+
+	private T[] buffer;
+	private boolean full;
+	private int start, end, maxLength;
+	
+	public Ringbuffer(int length) {
+		maxLength = length;
+		buffer = (T[]) new Object[maxLength];
+	}
+	
+	private int next(int n) {
+		return (n+1) % maxLength;
+	}
+	
+	public boolean isEmpty() {
+		return (start == end) && !full;
+	}
+	
+	public boolean isFull() {
+		return full;
+	}
+	
+	public void add(T t) throws Exception {
+		if (isFull())
+			remove();
+		
+		buffer[end] = t;
+		end = next(end);
+		full = (start == end);
+		
+//		System.out.println(t);
+	}
+	
+	public T remove() throws Exception {
+		if( isEmpty() ) throw new Exception("Array ist empty!");
+		
+		T t = buffer[start];
+		start = next(start);
+		full = false;
+		
+//		System.out.println(t);
+		
+		return t;
+	}
+	
+}
---
+++ src/edu/dhbw/t10/type/TestTree.java
@@ -0,0 +1,148 @@
+/* 
+ * *********************************************************
+ * Copyright (c) 2011 - 2011, DHBW Mannheim
+ * Project: T10 On-Screen Keyboard
+ * Date: Oct 20, 2011
+ * Author(s): dirk
+ *
+ * *********************************************************
+ */
+package edu.dhbw.t10.type;
+
+import edu.dhbw.t10.manager.ImportExportManager;
+
+
+
+
+
+
+
+/**
+ * TODO dirk, add comment!
+ * - What should this type do (in one sentence)?
+ * - If not intuitive: A simple example how to use this class
+ * 
+ * @author dirk
+ * 
+ */
+public class TestTree {
+	
+	/**
+	 * TO DELETE
+	 * only for testing the tree
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// BasicConfigurator.configure();
+		
+		
+		PriorityTree tree = new PriorityTree();
+		// tree.importFromHashMap(ImportExportManager.importFromFile("/home/dirk/Desktop/dict"));
+		// tree.printTree();
+		tree.importFromHashMap(ImportExportManager.importFromText("/home/dirk/Desktop/projektarbeiten"));
+		// System.out.println("Import completed");
+		// Profile prof = new Profile(1, "Pflichteheft", "/home/dirk/Desktop/PFL", tree);
+		// System.out.println("starting serializsation");
+		// prof.saveTree();
+		// System.out.println("finished - exporting");
+		// tree.importFromHashMap(ImportExportManager.importFromText("/home/dirk/Desktop/projektarbeiten"));
+		tree.autoCleaning(1, 20000000000000L, 3);
+		// tree.printTree();
+		for (PriorityElement pe : tree.getFreqSortedList())
+			pe.print();
+		// tree.autoCleaning(1, new Long(20000000 * 10000000), 0);
+		// System.out.println(tree.getSuggest("ha"));
+		// tree.printTree();
+		ImportExportManager.exportToFile(tree.exportToHashMap(), "/home/dirk/Desktop/dict");
+		// System.out.println("finished");
+		// System.out.println("1");
+		Profile prof = new Profile(1, "Pflichteheft", "/home/dirk/Desktop/PFL", tree);
+		// System.out.println("2");
+		prof.saveTree();
+		prof.setTree(new PriorityTree());
+		System.out.println(prof.getTree().getSuggest("ha"));
+		prof.loadTree();
+		System.out.println(prof.getTree().getSuggest("ha"));
+		// System.out.println("3");
+		// prof.setTree(new PriorityTree());
+		// System.out.println(prof.getTree().getSuggest("ha"));
+		// prof.loadTree();
+		// System.out.println(prof.getTree().getSuggest("ha"));
+		// System.out.println("Go");
+		// // for (int i = 0; i < 1000000; i++) {
+		// prof.getTree().getSuggest("ha");
+		// }
+		// System.out.println("1000000 times getSuggest");
+		// for (int i = 0; i < 100; i++) {
+		// prof.saveTree();
+		// prof.loadTree();
+		// }
+		// System.out.println("100 times load/saveTree");
+		// ImportManager imp = new ImportManager();
+		// imp.readFile("/home/dirk/Desktop/test");
+		// tree.importFromHashMap(imp.getImportedInfos());
+		// tree.printTree();
+		System.out.println(tree.getSuggest("No"));
+		System.out.println(tree.getSuggest("app"));
+		System.out.println(tree.getSuggest("cou"));
+		System.out.println(tree.getSuggest("be"));
+		System.out.println(tree.getSuggest("fou"));
+		System.out.println(tree.getSuggest("fo"));
+		System.out.println(tree.getSuggest("log"));
+		System.out.println(tree.getSuggest("Ple"));
+		System.out.println(tree.getSuggest("ini"));
+		System.out.println(tree.getSuggest("sys"));
+		System.out.println(tree.getSuggest("pro"));
+		System.out.println(tree.getSuggest("Jav"));
+		// tree.printTree("ic", false);
+		// System.out.println(tree.getSuggest("Das"));
+		// System.out.println(tree.getSuggest("Arc"));
+		// System.out.println(tree.getSuggest("bei"));
+		// System.out.println(tree.getSuggest("die"));
+		// System.out.println(tree.getSuggest("Arc"));
+		// System.out.println(tree.getSuggest("für"));
+		// System.out.println(tree.getSuggest("uns"));
+		// System.out.println(tree.getSuggest("Pro"));
+		// System.out.println(tree.getSuggest("Bil"));
+		// System.out.println(tree.getSuggest("Tas"));
+		// System.out.println(tree.getSuggest("mit"));
+		// System.out.println(tree.getSuggest("Pri"));
+		// System.out.println(tree.getSuggest("und"));
+		// System.out.println(tree.getSuggest("Obe"));
+
+		
+		// tree.insert("hallo");
+		// tree.insert("test");
+		// tree.insert("holla");
+		// tree.insert("holla");
+		// tree.insert("holoo");
+		// tree.insert("holoo");
+		// tree.insert("holoo");
+		// tree.insert("ich");
+		// tree.printTree();
+		// tree.delete("holla");
+		// tree.printTree();
+		// System.out.println(tree.getSuggest("h"));
+
+	}
+	// --------------------------------------------------------------------------
+	// --- variables and constants ----------------------------------------------
+	// --------------------------------------------------------------------------
+	
+	
+	// --------------------------------------------------------------------------
+	// --- constructors ---------------------------------------------------------
+	// --------------------------------------------------------------------------
+	
+	
+	// --------------------------------------------------------------------------
+	// --- methods --------------------------------------------------------------
+	// --------------------------------------------------------------------------
+	
+	
+	// --------------------------------------------------------------------------
+	// --- getter/setter --------------------------------------------------------
+	// --------------------------------------------------------------------------
+}
