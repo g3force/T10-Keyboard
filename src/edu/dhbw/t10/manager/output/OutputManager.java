b84ced82a9a50a5f4c7f637a9326eafacc2b39ba
Daniel Andres <daniel@laptop>
2011-10-28 11:34:25 +0200

    Zwischenpush vom Controller, OutputManager und Output
    Change-Id: Ic3c37ec737fe9440f96cb33262fde7d6e4c97a8e

--- src/edu/dhbw/t10/manager/output/OutputManager.java
+++ src/edu/dhbw/t10/manager/output/OutputManager.java
@@ -1,27 +1,27 @@
-/* 
+/*
  * *********************************************************
  * Copyright (c) 2011 - 2011, DHBW Mannheim
  * Project: T10 On-Screen Keyboard
  * Date: Oct 15, 2011
  * Author(s): NicolaiO
- *
+ * 
  * *********************************************************
  */
 package edu.dhbw.t10.manager.output;
 
 import org.apache.log4j.Logger;
 
+import edu.dhbw.t10.type.keyboard.Key;
+
 
 /**
  * TODO NicolaiO, add comment!
- * - What should this type do (in one sentence)?
- * - If not intuitive: A simple example how to use this class
+ * 
  * 
  * @author Andres
  * 
  */
-public class OutputManager
-{
+public class OutputManager {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
@@ -30,6 +30,7 @@ public class OutputManager
 	Output								out		= Output.getInstance();
 	private static OutputManager	instance	= null;
 	
+
 	// --------------------------------------------------------------------------
 	// --- constructors ---------------------------------------------------------
 	// --------------------------------------------------------------------------
@@ -38,21 +39,43 @@ public class OutputManager
 	}
 
 
-
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
-	public static OutputManager getInstance() {
-		if (instance == null) {
-			instance = new OutputManager();
+	
+	public boolean printChar(Key c) {
+		return out.printChar(c);
+	}
+	
+
+	public boolean deleteChar(int length) {
+		if (length <= 0)
+			return false;
+		else {
+			for (int i = 0; i < length; i++) {
+				out.printString("\\BACK_SPACE\\", Key.CONTROL_KEY);
+			}
+			return true;
 		}
-		return instance;
+	}
+
+
+	public boolean printString(String input) {
+		return out.printString(input);
+	}
+	
+
+	public boolean mark(int i) {
+		// FIXME Logik von Output hierherkopieren....
+		return out.markChar(i);
+	}
+	
+
+	public void unMark() {
+		out.printString("\\RIGHT\\", Key.CONTROL_KEY);
 	}
 
 	
-
-	
-
 	/**
 	 * 
 	 * printSuggest deletes the old suggest, prints it out and mark the chars that are added from the suggest word.
@@ -64,9 +87,37 @@ public class OutputManager
 	}
 	
 
-	public String convertToUnicode(String input) {
-		// TODO
-		return input;
+	/**
+	 * 
+	 * TODO andres, add comment!
+	 * Quelle: http://www.daniweb.com/software-development/java/threads/147397
+	 * modified
+	 * 
+	 * FIXME evtl. diese Funktion in Output.class und mit extractUnicode kombinieren
+	 * 
+	 * @param input
+	 * @return
+	 * @author andres
+	 */
+	private String convertToUnicode(String input) {
+		StringBuffer ostr = new StringBuffer();
+		
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			
+			if ((ch >= 0x0020) && (ch <= 0x007e)) { // Does the char need to be converted to unicode?
+				ostr.append(ch); // No.
+			} else { // Yes.
+				ostr.append("\\u+"); // own unicode format
+				String hex = Integer.toHexString(input.charAt(i) & 0xFFFF); // Get hex value of the char.
+				for (int j = 0; j < 4 - hex.length(); j++)
+					// Prepend zeros because unicode requires 4 digits
+					ostr.append("0");
+				ostr.append(hex.toLowerCase()); // standard unicode format.
+				ostr.append("\\"); // own unicode format
+			}
+		}
+		return (new String(ostr)); // Return the stringbuffer cast as a string.
 	}
 
 
@@ -74,7 +125,10 @@ public class OutputManager
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
 	
-	public Output getOutput() {
-		return out;
+	public static OutputManager getInstance() {
+		if (instance == null) {
+			instance = new OutputManager();
+		}
+		return instance;
 	}
 }
