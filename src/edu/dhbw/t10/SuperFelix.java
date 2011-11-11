0e537ef3120b99e5b4aa61f2b28d69eaef2cd7fc
Daniel Andres <daniel@laptop>
2011-11-11 21:33:03 +0100

    PrintCombi (unsauber, hat ein FIXME), Comments...
    Change-Id: I3b6e123908849b96aef6dda719f8cc13031f67ff

--- src/edu/dhbw/t10/SuperFelix.java
+++ src/edu/dhbw/t10/SuperFelix.java
@@ -24,7 +24,7 @@ import edu.dhbw.t10.manager.Controller;
  * 
  * TODO OPTIONAL ALL graphical buttons (keys) e.g. pause- and arrow keys
  * TODO FelixP menus
- * TODO OPTIONAL ALL StatusBar
+ * TODO ALL use the Right Statusbar for Tooltips...
  * TODO ALL question: insert word that is smaller than an existing one?!
  * TODO NicolaiO Toolbar
  * 
