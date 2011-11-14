809b5f002f1e01ee2392fd31e3fb9a8ee7e6a1d9
PEACE121 <dirk@klm-on.de>
2011-11-14 15:30:43 +0100

    serialize bug solved

--- src/edu/dhbw/t10/manager/profile/ProfileManager.java
+++ src/edu/dhbw/t10/manager/profile/ProfileManager.java
@@ -453,6 +453,7 @@ public class ProfileManager {
 			Profile cProfile = profiles.get(i);
 			if (cProfile.getPathToProfile() == null || cProfile.getPathToProfile().isEmpty())
 				continue;
+			logger.info("Serializing Porfile " + cProfile.getName() + " to " + cProfile.getPathToProfile());
 			try {
 				Serializer.serialize(cProfile, cProfile.getPathToProfile());
 			} catch (IOException io) {
