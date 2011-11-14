3cb4fe972da25ef55a78ef62f93e25baa2d81be1
Daniel Andres <daniel@laptop>
2011-11-14 21:24:59 +0100

    Graphical buttons resize --> google code issue
    Change-Id: I0a4f6fec0536222832099e5740a2999956269c4f

--- src/edu/dhbw/t10/SuperFelix.java
+++ src/edu/dhbw/t10/SuperFelix.java
@@ -22,7 +22,6 @@ import edu.dhbw.t10.manager.Controller;
 /**
  * This is the main class of the t10 keyboard. It only initializes the first important classes.
  * 
- * TODO OPTIONAL ALL graphical buttons --> resize is not implemented...
  * TODO FelixP optional menu item "Modify"
  * TODO FelixP Export Profile nach Profile verschieben (gehört schließlich zum aktuellen profile)
  * TODO ALL Read: Ich habe die Symphony Kompatibilität hergestellt. allerdings geht das auf Kosten von Performance...
