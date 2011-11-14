324772ddfc27161e850a9a1e4f8473ab4d45d170
felix <felix@thinkpad>
2011-11-14 13:39:06 +0100

    ...

--- src/edu/dhbw/t10/view/dialogs/ProfileCleanerDlg.java
+++ src/edu/dhbw/t10/view/dialogs/ProfileCleanerDlg.java
@@ -20,7 +20,6 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 import edu.dhbw.t10.manager.Controller;
-import edu.dhbw.t10.type.profile.Profile;
 import edu.dhbw.t10.view.menus.EMenuItem;
 
 
@@ -90,11 +89,6 @@ public class ProfileCleanerDlg extends JDialog {
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
-	public Profile getProfile() {
-		return null;
-	}
-	
-
 	public Integer getFrequency() {
 		Integer i = (Integer) spinField.getValue();
 		return i;
