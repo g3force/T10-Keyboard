278fafac6b679b3b2d037d50a6ba35c906565e86
Nicolai Ommer <nicolai.ommer@gmail.com>
2011-11-15 11:26:52 +0100

    removed TODOs

--- src/edu/dhbw/t10/SuperFelix.java
+++ src/edu/dhbw/t10/SuperFelix.java
@@ -23,11 +23,8 @@ import edu.dhbw.t10.manager.Controller;
  * This is the main class of the t10 keyboard. It only initializes the first important classes.
  * 
  * TODO FelixP optional menu item "Modify"
- * TODO DanielAl CAPS keycode?? for system LED? :P Another idea? delete caps? with what to be replaced??
- * TODO ALL Read: Ich habe die Symphony Kompatibilität hergestellt. allerdings geht das auf Kosten von Performance...
  * TODO FelixP neue Menüstruktur File-> Keyboard, Help mit About Dialog hinzufügen
- * Jetzt wird ein Vorschlag, wenn er accepted wird, nicht mehr demarkiert, sondern neu geschrieben.
- * Wenn gelesen, löschen! *
+ * 
  * @author NicolaiO, DanielAl, FelixP, DirkK, SebastianN
  * 
  */
