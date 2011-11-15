2662673c74cbf5a3829c59c27553a9ad072b733f
felix <felix@thinkpad>
2011-11-15 14:57:18 +0100

    Grundlage: link zu FB und G+

--- src/edu/dhbw/t10/view/dialogs/AboutDlg.java
+++ src/edu/dhbw/t10/view/dialogs/AboutDlg.java
@@ -12,6 +12,7 @@ package edu.dhbw.t10.view.dialogs;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -33,8 +34,9 @@ public class AboutDlg extends JDialog {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
-	JButton		likeBtn;
-	ImageIcon	icon;
+	private JButton	likeBtn;
+	private ImageIcon	icon;
+	private String		path;
 	
 
 	// --------------------------------------------------------------------------
@@ -42,6 +44,7 @@ public class AboutDlg extends JDialog {
 	// --------------------------------------------------------------------------
 	public AboutDlg() {
 		this.setTitle("About");
+		path = "http://www.facebook.com/UseAcc";
 
 		likeBtn = new JButton("like");
 		likeBtn.addActionListener(new ActionListener() {
@@ -67,7 +70,30 @@ public class AboutDlg extends JDialog {
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
+	private void openBrowser() {
+		final String os = System.getProperty("os.name").toLowerCase();
+		try {
+			if (os.indexOf("mac") >= 0) {
+				Runtime.getRuntime().exec("open " + path);
+			}
 
+			if (os.indexOf("windows") >= 0) {
+				if (os.equals("windows nt")) {
+					Runtime.getRuntime().exec("cmd.exe /C " + path);
+				} else if (os.equals("windows 95")) {
+					Runtime.getRuntime().exec("command.com /C " + path);
+				} else {
+					Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + path);
+				}
+			}
+			
+			// x-www-browser -newwindow -fullscreen "http://www.facebook.com/UseAcc"
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
