9bc60c6d4979c44a87af468fbd1cefec69cb08e8
Nicolai Ommer <nicolai.ommer@gmail.com>
2011-11-15 12:02:45 +0100

    loading Images from layout, fixed small error in DDL

--- src/edu/dhbw/t10/type/keyboard/Image.java
+++ src/edu/dhbw/t10/type/keyboard/Image.java
@@ -1,115 +1,58 @@
-/*
+/* 
  * *********************************************************
  * Copyright (c) 2011 - 2011, DHBW Mannheim
  * Project: T10 On-Screen Keyboard
- * Date: Oct 24, 2011
+ * Date: Nov 15, 2011
  * Author(s): NicolaiO
- * 
+ *
  * *********************************************************
  */
 package edu.dhbw.t10.type.keyboard;
 
 import java.awt.Dimension;
 
-import javax.swing.JComboBox;
-
 
 /**
- * Drop down list on the keyboard, especially for profile dropdown
+ * TODO NicolaiO, add comment!
+ * - What should this type do (in one sentence)?
+ * - If not intuitive: A simple example how to use this class
  * 
  * @author NicolaiO
  * 
  */
-public class DropDownList extends JComboBox {
-	private static final long	serialVersionUID	= 5433669829791012176L;
+public class Image {
 	// --------------------------------------------------------------------------
 	// --- variables and constants ----------------------------------------------
 	// --------------------------------------------------------------------------
-	public static final int		PROFILE				= 0;
-	private int						type					= PROFILE;
-	private Dimension				origSize				= new Dimension(10, 10);
-	private int						pos_x					= 0;
-	private int						pos_y					= 0;
 	
-	
+	private Dimension	origSize	= new Dimension(10, 10);
+	private int			pos_x		= 0;
+	private int			pos_y		= 0;
+	private String		src		= "";
+
+
 	// --------------------------------------------------------------------------
 	// --- constructors ---------------------------------------------------------
 	// --------------------------------------------------------------------------
-	/**
-	 * Create a new DropDownList with given type and bounds.
-	 * 
-	 * @param type
-	 * @param size_x
-	 * @param size_y
-	 * @param pos_x
-	 * @param pos_y
-	 * @author NicolaiO
-	 */
-	public DropDownList(int type, int size_x, int size_y, int pos_x, int pos_y) {
-		init(type, size_x, size_y, pos_x, pos_y);
+	
+	public Image(String src, int size_x, int size_y, int pos_x, int pos_y) {
+		init(src, size_x, size_y, pos_x, pos_y);
 	}
-	
-	
-	/**
-	 * 
-	 * @param type
-	 * @param size_x
-	 * @param size_y
-	 * @param pos_x
-	 * @param pos_y
-	 */
-	public DropDownList(String type, int size_x, int size_y, int pos_x, int pos_y) {
-		if (type.equals("profile")) {
-			this.type = PROFILE;
-		}
-		init(this.type, size_x, size_y, pos_x, pos_y);
-	}
-	
 	
 	// --------------------------------------------------------------------------
 	// --- methods --------------------------------------------------------------
 	// --------------------------------------------------------------------------
 	
-	/**
-	 * Initialize DDL with type, size and position
-	 * 
-	 * @param type
-	 * @param size_x
-	 * @param size_y
-	 * @param pos_x
-	 * @param pos_y
-	 * @author NicolaiO
-	 */
-	private void init(int type, int size_x, int size_y, int pos_x, int pos_y) {
-		this.type = type;
+	private void init(String src, int size_x, int size_y, int pos_x, int pos_y) {
 		this.origSize = new Dimension(size_x, size_y);
 		this.pos_x = pos_x;
 		this.pos_y = pos_y;
-		setLayout(null);
-		setBounds(getPos_x(), getPos_y(), getSize().width, getSize().height);
+		this.src = src;
 	}
-	
 	
 	// --------------------------------------------------------------------------
 	// --- getter/setter --------------------------------------------------------
 	// --------------------------------------------------------------------------
-	
-	public int getType() {
-		return type;
-	}
-	
-	
-	public void setType(int type) {
-		this.type = type;
-	}
-	
-	
-	public String getTypeAsString() {
-		if (type == DropDownList.PROFILE)
-			return "profile";
-		return "";
-	}
-	
 	
 	public Dimension getOrigSize() {
 		return origSize;
@@ -138,5 +81,15 @@ public class DropDownList extends JComboBox {
 	
 	public void setPos_y(int pos_y) {
 		this.pos_y = pos_y;
+	}
+	
+	
+	public String getSrc() {
+		return src;
+	}
+	
+	
+	public void setSrc(String src) {
+		this.src = src;
 	}
 }
