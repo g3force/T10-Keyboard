955c263211cb02e15ae46b2f5b779b6229386865
Nicolai Ommer <g3force@online.de>
2011-11-21 11:14:37 +0100

    packaging script will now automatically change REV in code!

--- tools/generateDeb.sh
+++ tools/generateDeb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ ! $# -eq 1 ]; then
-  echo "Usage: ./generateDeb.sh <version>"
-  echo "version should look like 0.8 (not with V and without revision!)" 
-  return 1
-fi
+#if [ ! $# -eq 1 ]; then
+#  echo "Usage: ./generateDeb.sh <version>"
+#  echo "version should look like 0.8 (not with V and without revision!)" 
+#  return 1
+#fi
 if [ ! -d .git ]; then 
   echo "You are not in the repository dir! Please go there ;)"
   return 2
@@ -18,11 +18,19 @@ else
   echo "jar file should be located in repo root..."
 fi
 
-# change version
-revision="r`git shortlog | grep -E '^[ ]+\w+' | wc -l`"
+# get some values
+version="`cat src/edu/dhbw/t10/SuperFelix.java | grep -E 'String.*VERSION' | sed 's/[^0-9.]//g'`"
+revision="`git shortlog | grep -E '^[ ]+\w+' | wc -l`"
 control="`cat debian/DEBIAN/control | grep -v Version:`"
-echo "${control}\nVersion: $1-$revision">debian/DEBIAN/control
 
-outputFile="t10-keyboard-linux-V$1-$revision.deb"
+# change version
+perl -pi -w -e 's/REV[ ]*[\t]*\=[ ]*[\t]*"[0-9]*"/REV \t\t= \"'$revision'\"/g;' src/edu/dhbw/t10/SuperFelix.java
+#    -e means execute the following line of code.
+#    -i means edit in-place
+#    -w write warnings
+#    -p loop
+echo "${control}\nVersion: $version-$revision">debian/DEBIAN/control
+
+outputFile="t10-keyboard-linux-V$version-r$revision.deb"
 dpkg-deb --build debian/ $outputFile
 echo "OutputFile: $outputFile"
